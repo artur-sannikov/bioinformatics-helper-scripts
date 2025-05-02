a892cbd9b581ec19a3a712df04834546aa374fff
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2025-05-02 11:19:39 +0300

    feat: Check md5 sum of MetaPhlAn database

--- bash/download_databases.sh
+++ bash/download_databases.sh
@@ -27,6 +27,23 @@ download_file() {
     echo -e "Download ${GREEN}successful${NC}!"
 }
 
+check_md5() {
+    local file="$1"
+    local expected_md5="$2"
+
+    local computed_md5
+    computed_md5=$(md5sum "$file" | awk '{print $1}')
+
+    if [[ "$computed_md5" != "$expected_md5" ]]; then
+        echo "MD5 mismatch for '{$file}'"
+        echo "Expected: '${expected_md5}'"
+        echo "Got: '${computed_md5}'"
+        exit 1
+    else
+        echo "MD5 checksum passed for '${file}'"
+    fi
+}
+
 # Download Mus musculus genome
 URL="https://ftp.ensembl.org/pub/release-113/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.toplevel.fa.gz"
 download_file "$URL" "Mus musculus genome"
@@ -43,3 +60,6 @@ METAPHLAN_DB_DIR="$OUTPUT_DIR/${FILENAME%.tar}"
 echo $METAPHLAN_DB_DIR
 mkdir -p "$METAPHLAN_DB_DIR"
 tar xf "$OUTPUT_DIR/$FILENAME" -C "$METAPHLAN_DB_DIR"
+
+# Check MetaPhlAn md5 checksum
+check_md5 "$OUTPUT_DIR/$FILENAME" "90277ac04de6c72d6542f75bdb1b5104"
